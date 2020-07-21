e7a0e1434a272342b74199ea6ae8d147a5448512
Norbert Korom <norbertkorom@gmail.com>
2020-07-21 12:19:22 +0100

    Update main.js
    setup bootstrap, fontawesome, vee-validate

--- src/main.js
+++ src/main.js
@@ -2,6 +2,30 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import * as VeeValidate from 'vee-validate'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faTimes,
+  faUser,
+  faUserPlus,
+  faSignInAlt,
+  faSignOutAlt,
+  faUserShield
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faTimes,
+    faUser,
+    faUserPlus,
+    faSignInAlt,
+    faSignOutAlt,
+    faUserShield)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(VeeValidate)
 
 Vue.config.productionTip = false
 
